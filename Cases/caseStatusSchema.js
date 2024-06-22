e0a9a361b5ea097b7277ce6c41134b34c19d267e
soumyasics <soumya.sics@gmail.com>
2024-06-22 16:40:02 +0530

    p

---
+++ Cases/caseStatusSchema.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const statusSchema = mongoose.Schema({
+    caseId:{
+        type:mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref:'cases'
+
+    },
+    userId: {
+        type:mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref:"users"
+    },
+    
+        advocateId:{
+            type:mongoose.Schema.Types.ObjectId,
+            default:null,
+            ref:'advocates'
+
+        },
+        status:{
+            type:String,
+            required: true,
+        },
+        date:{
+            type:Date,
+        }, 
+        description:{
+            type:String
+        }
+
+});
+module.exports = mongoose.model('casestatus', statusSchema)
+
--- routes.js
+++ routes.js
@@ -1,5 +1,8 @@
 const router=require('express').Router()
+const user=require('./User/userController')
 const advocates=require('./Advocates/advocateController')
+const cases=require('./Cases/caseController')
+const appointments=require('./AdvAppointments/appointmentController')
 
 
 //advocate routes
@@ -7,7 +10,7 @@ router.post('/registerAdvocate',advocates.upload,advocates.registerAdvocate)
 router.post('/viewAdvocateById/:id',advocates.viewAdvocateById)
 router.post('/forgotPassword',advocates.forgotPassword)
 router.post('/loginAdvocate',advocates.login)
-router.post('/editAdvocateById/:id',advocates.upload,advocates.editAdvocateById)
+router.post('/editAdvocateById/:id',advocates.uploadProfile,advocates.editAdvocateById)
 router.post('/deleteAdvocateById/:id',advocates.deleteAdvocateById)
 router.post('/resetPassword/:id',advocates.resetPassword)
 router.post('/approveAdvocateById/:id',advocates.approveAdvocateById)
@@ -15,5 +18,32 @@ router.post('/rejectAdvocateById/:id',advocates.rejectAdvocateById)
 router.post('/requireAuth',advocates.requireAuth)
 router.post('/viewAdvocateReqs',advocates.viewAdvocateReqs)
 router.post('/viewAdvocates',advocates.viewAdvocates)
+router.post('/activateAdvocateById/:id',advocates.activateAdvocateById)
+router.post('/deactivateAdvocateById/:id',advocates.deactivateAdvocateById)
+
+
+//users routers
+router.post('/registerUser',user.upload,user.registerUser)
+router.post('/loginUser',user.login)
+router.post('/editUserById/:id',user.upload,user.editUserById)
+router.post('/viewUserById/:id',user.viewUserById)
+router.post('/forgotPassword',user.forgotPassword)
+router.post('/resetPassword',user.resetPassword)
+router.post('/deleteUserById/:id',user.deleteUserById)
+router.post('/editUserById',user.upload,user.login)
+router.post('/requireAuth',user.requireAuth)
+router.post('/viewUsers',user.viewUsers)
+
+//case routes
+router.post('/createCase/:id',cases.upload,cases.createCase)
+router.post('/getCaseType',cases.getCaseType)
+router.post('/getCaseByUserId/:id',cases.getCaseByUserId)
+
+router.post('/createAppointment',appointments.createAppointment)
+router.post('/getAppointmentReqsByUserId/:id',appointments.getAppointmentReqsByUserId)
+router.post('/getAppointmentReqsForAdv/:id',appointments.getAppointmentReqsForAdv)
+router.post('/acceptReqbyAdv/:id',appointments.acceptReqbyAdv)
+router.post('/rejectReqbyAdv/:id',appointments.rejectReqbyAdv)
+router.post('/getAppointmentReqsById/:id',appointments.getAppointmentReqsById)
 
 module.exports=router
